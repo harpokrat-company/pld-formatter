fc3b3dd39f086257cbf9cb12e6b44492a0f3c953
Antoine Drouot <antoine@antoine.drouot>
2020-01-15 21:25:24 +0000

    Ajout du parsing CSV + flat array to tree

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 
+const csv = require('csv-parser');
+const _ = require('lodash');
+const fs = require('fs');
+const content = [];
+const arrayToTree = require('array-to-tree');
+
+const config = {
+    separator: '.',
+};
+
+
+
 function main() {
-    return 0
+    fs.createReadStream('test.csv')
+        .pipe(csv(config))
+        .on('data', (data) => {
+            data.id = parseInt(data.id, 10);
+            data.parent_id = parseInt(data.Parent.replace(/\D/g, ''), 10) || undefined;
+            //content.push({ ...data, children: [] })
+            content.push(data);
+        })
+        .on('end', () => {
+            tree = arrayToTree(content);
+            // Balancer le tree en HTML
+        });
 }
 
-main()
+main();
