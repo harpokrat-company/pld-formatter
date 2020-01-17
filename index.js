97522c1e2d3967f10917d0719e5f1f6c6aaa670e
Fantin Bibas <fantin.bibas@epitech.eu>
2020-01-17 14:23:46 +0000

    Clean project base

--- index.js
+++ index.js
@@ -1,29 +1,5 @@
+const parseCSV = require('./parsing.js');
 
-const csv = require('csv-parser');
-const _ = require('lodash');
-const fs = require('fs');
-const content = [];
-const arrayToTree = require('array-to-tree');
-
-const config = {
-    separator: '.',
+module.exports = {
+    parseCSV,
 };
-
-
-
-function main() {
-    fs.createReadStream('test.csv')
-        .pipe(csv(config))
-        .on('data', (data) => {
-            data.id = parseInt(data.id, 10);
-            data.parent_id = parseInt(data.Parent.replace(/\D/g, ''), 10) || undefined;
-            //content.push({ ...data, children: [] })
-            content.push(data);
-        })
-        .on('end', () => {
-            tree = arrayToTree(content);
-            // Balancer le tree en HTML
-        });
-}
-
-main();
---
+++ parsing.js
@@ -0,0 +1,29 @@
+const csv = require('csv-parser');
+const arrayToTree = require('array-to-tree');
+
+export default (raw_csv) => {
+    const flat_csv = [];
+    const parser = csv(
+        {
+            mapHeaders: ({header}) => header
+                .toLowerCase()
+                .replace(' ', '-')
+                .replace(/[^0-9a-z\-]/gi, ''), // Slugify column header
+            separator: ',',
+        });
+    const promise = new Promise((resolve) => {
+        parser
+            .on('data', (data) => {
+                data.id = parseInt(data.id, 10);
+                data.parent_id = parseInt(data.parent.replace(/\D/g, ''), 10) || undefined;
+                flat_csv.push(data);
+            })
+            .on('end', () => {
+                const tree = arrayToTree(flat_csv);
+                resolve(tree);
+            });
+    });
+    parser.write(raw_csv);
+    parser.end();
+    return promise;
+};
